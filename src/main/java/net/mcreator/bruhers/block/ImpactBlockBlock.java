b13b598b8df8fc6fc15e3e9d8f2f1286490fbf00
kevinkor01 <konzo@DESKTOP-M8U0KG2>
2021-02-11 17:06:08 +0200

    Booting

---
+++ src/main/java/net/mcreator/bruhers/block/ImpactBlockBlock.java
@@ -0,0 +1,50 @@
+
+package net.mcreator.bruhers.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.bruhers.item.ImpactitemItem;
+import net.mcreator.bruhers.BruhersModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@BruhersModElements.ModElement.Tag
+public class ImpactBlockBlock extends BruhersModElements.ModElement {
+	@ObjectHolder("bruhers:impact_block")
+	public static final Block block = null;
+	public ImpactBlockBlock(BruhersModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("impact_block");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(ImpactitemItem.block, (int) (1)));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/bruhers/item/ImpactitemItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.bruhers.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.bruhers.BruhersModElements;
+
+@BruhersModElements.ModElement.Tag
+public class ImpactitemItem extends BruhersModElements.ModElement {
+	@ObjectHolder("bruhers:impactitem")
+	public static final Item block = null;
+	public ImpactitemItem(BruhersModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("impactitem");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
